1f0f6d3309e9cd118ab721fa85e3b92a5333979c
twwct <twwc.tech@gmail.com>
2023-10-12 21:46:50 -0600

    Add method completed; not yet tested

--- Backend/UserManagement/WinUsersManagement.cs
+++ Backend/UserManagement/WinUsersManagement.cs
@@ -4,11 +4,45 @@ namespace UserCreator.Backend.UserManagement
 {
     internal class WinUsersManagement
     {
-        public void CreateNewUser(string username, bool admin)
-        {
-            using (DirectoryEntry localMachine = new(/* add param */))
-            {
+        private string? _localMachineEnvironement;
 
+        public WinUsersManagement(string localMachineEnvironment)
+        {
+            _localMachineEnvironement = localMachineEnvironment;
+        }
+        public void CreateNewUser(string username, string password, bool admin)
+        {
+            using (DirectoryEntry localMachine = new(_localMachineEnvironement))
+            {
+                using (DirectoryEntry newUser = localMachine.Children.Add(username, "user"))
+                {
+                    try
+                    {
+                        newUser.Invoke("SetPassword", new object[] { password });
+                        newUser.Invoke("Put", new object[] { "Description", "New Local User" });
+                        newUser.CommitChanges();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(ex.ToString());
+                    }
+                }
+
+                if (admin)
+                {
+                    using (DirectoryEntry adminGroup = localMachine.Children.Find("Administrators", "group"))
+                    {
+                        try
+                        {
+                            adminGroup.Invoke("Add", new object[] { $"{localMachine.Path}/{username}" });
+                            adminGroup.CommitChanges();
+                        }
+                        catch (Exception ex)
+                        {
+                            throw new Exception(ex.ToString());
+                        }
+                    }
+                }
             }
         }
 
