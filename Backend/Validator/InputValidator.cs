e2340b064b8af864e5c57519c189c71efea3e3d3
twwct <twwc.tech@gmail.com>
2023-10-15 21:21:32 -0600

    new file name

---
+++ Backend/Validator/InputValidator.cs
@@ -0,0 +1,23 @@
+﻿namespace UserCreator.Backend.Validator
+{
+    internal class InputValidator
+    {
+        public static bool AreTextboxesEmpty(TextBox[] textboxes)
+        {
+            int emptyTextboxes = textboxes.Length;
+            for (int textboxIndex = 0; textboxIndex < emptyTextboxes; textboxIndex++)
+            {
+                if (!string.IsNullOrWhiteSpace(textboxes[textboxIndex].Text))
+                {
+                    emptyTextboxes--;
+                }
+            }
+
+            if (emptyTextboxes == 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
