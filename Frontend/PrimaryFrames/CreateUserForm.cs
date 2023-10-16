dadc97e1c637bc96a7e5c0b8effe16c4fb799c39
twwct <twwc.tech@gmail.com>
2023-10-15 21:59:42 -0600

    button method created; not yet tested

--- Frontend/PrimaryFrames/CreateUserForm.cs
+++ Frontend/PrimaryFrames/CreateUserForm.cs
@@ -1,4 +1,8 @@
-﻿namespace UserCreator.Frontend.PrimaryFrames
+﻿using UserCreator.AppResources;
+using UserCreator.Backend.UserManagement;
+using UserCreator.Backend.Validator;
+
+namespace UserCreator.Frontend.PrimaryFrames
 {
     public partial class CreateUserForm : Form
     {
@@ -14,7 +18,17 @@
 
         private void CreateButton_Click(object sender, EventArgs e)
         {
-
+            TextBox[] textBoxes = { NewUsernameTextbox, NewPasswordTextbox };
+            bool areTextboxesEmpty = InputValidator.AreTextboxesEmpty(textBoxes);
+            if (areTextboxesEmpty)
+            {
+                MessageBox.Show("Entries must not be empty", "Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                WinUsersManagement userManagement = new(Statics.LocalMachineEnvironment);
+                userManagement.CreateNewUser(NewUsernameTextbox.Text.Trim(), NewPasswordTextbox.Text.Trim(), AdminCheckbox.Checked);
+            }
         }
     }
 }
