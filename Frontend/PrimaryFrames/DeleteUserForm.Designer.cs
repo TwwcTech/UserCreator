bb68afdfd5d7d6b103495be379b761bf1f36f886
twwct <twwc.tech@gmail.com>
2023-10-15 21:23:23 -0600

    Properties updated

--- Frontend/PrimaryFrames/DeleteUserForm.Designer.cs
+++ Frontend/PrimaryFrames/DeleteUserForm.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DeleteUserForm";
+            SuspendLayout();
+            // 
+            // DeleteUserForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "DeleteUserForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Delete User";
+            ResumeLayout(false);
         }
 
         #endregion
