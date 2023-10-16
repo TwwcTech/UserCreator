bc920ddd85cf7e66ec69edba7a385bfea109eee3
twwct <twwc.tech@gmail.com>
2023-10-15 21:21:48 -0600

    Create User Form updated

--- Frontend/PrimaryFrames/CreateUserForm.Designer.cs
+++ Frontend/PrimaryFrames/CreateUserForm.Designer.cs
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CreateUserForm";
+            SuspendLayout();
+            // 
+            // CreateUserForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "CreateUserForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Create User";
+            ResumeLayout(false);
         }
 
         #endregion
