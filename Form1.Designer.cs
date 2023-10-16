0066955c9381811d3264ab071a663403f0b08ed6
twwct <twwc.tech@gmail.com>
2023-10-15 20:23:43 -0600

    buttons added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,21 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            CreateUserButton = new Button();
+            UpdateUserButton = new Button();
+            DeleteUserButton = new Button();
             SuspendLayout();
+            // 
+            // CreateUserButton
+            // 
+            CreateUserButton.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            CreateUserButton.Location = new Point(147, 198);
+            CreateUserButton.Name = "CreateUserButton";
+            CreateUserButton.Size = new Size(134, 49);
+            CreateUserButton.TabIndex = 0;
+            CreateUserButton.Text = "Create User";
+            CreateUserButton.UseVisualStyleBackColor = true;
+            // 
+            // UpdateUserButton
+            // 
+            UpdateUserButton.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            UpdateUserButton.Location = new Point(331, 198);
+            UpdateUserButton.Name = "UpdateUserButton";
+            UpdateUserButton.Size = new Size(134, 49);
+            UpdateUserButton.TabIndex = 1;
+            UpdateUserButton.Text = "Update User";
+            UpdateUserButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteUserButton
+            // 
+            DeleteUserButton.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            DeleteUserButton.Location = new Point(513, 198);
+            DeleteUserButton.Name = "DeleteUserButton";
+            DeleteUserButton.Size = new Size(134, 49);
+            DeleteUserButton.TabIndex = 2;
+            DeleteUserButton.Text = "Delete User";
+            DeleteUserButton.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(DeleteUserButton);
+            Controls.Add(UpdateUserButton);
+            Controls.Add(CreateUserButton);
             FormBorderStyle = FormBorderStyle.Fixed3D;
             Name = "MainForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "User Creator";
+            Text = "User Creator Console";
             Load += MainForm_Load;
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button CreateUserButton;
+        private Button UpdateUserButton;
+        private Button DeleteUserButton;
     }
 }
