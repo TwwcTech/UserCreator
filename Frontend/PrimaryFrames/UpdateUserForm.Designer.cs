2a958c4f1bd1e0863ddbbfae690c61d9ce381c5e
twwct <twwc.tech@gmail.com>
2023-10-15 21:25:42 -0600

    Properties Updated

--- Frontend/PrimaryFrames/UpdateUserForm.Designer.cs
+++ Frontend/PrimaryFrames/UpdateUserForm.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "UpdateUserForm";
+            SuspendLayout();
+            // 
+            // UpdateUserForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "UpdateUserForm";
+            SizeGripStyle = SizeGripStyle.Hide;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Update User";
+            ResumeLayout(false);
         }
 
         #endregion
