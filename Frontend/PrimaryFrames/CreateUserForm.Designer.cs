0b3da2902ad522243d857aaecac4a31d7d58ac6e
twwct <twwc.tech@gmail.com>
2023-10-22 21:09:37 -0600

    GroupPanelBox added

--- Frontend/PrimaryFrames/CreateUserForm.Designer.cs
+++ Frontend/PrimaryFrames/CreateUserForm.Designer.cs
@@ -33,12 +33,14 @@
             AdminCheckbox = new CheckBox();
             CreateButton = new Button();
             DescriptionTextbox = new TextBox();
+            OptionsControlPanel = new GroupBox();
+            OptionsControlPanel.SuspendLayout();
             SuspendLayout();
             // 
             // NewUsernameTextbox
             // 
             NewUsernameTextbox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            NewUsernameTextbox.Location = new Point(207, 66);
+            NewUsernameTextbox.Location = new Point(207, 51);
             NewUsernameTextbox.Name = "NewUsernameTextbox";
             NewUsernameTextbox.PlaceholderText = "Enter New User Name";
             NewUsernameTextbox.Size = new Size(376, 27);
@@ -47,8 +49,9 @@
             // NewPasswordTextbox
             // 
             NewPasswordTextbox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            NewPasswordTextbox.Location = new Point(207, 130);
+            NewPasswordTextbox.Location = new Point(207, 115);
             NewPasswordTextbox.Name = "NewPasswordTextbox";
+            NewPasswordTextbox.PasswordChar = '*';
             NewPasswordTextbox.PlaceholderText = "Enter New Password";
             NewPasswordTextbox.Size = new Size(376, 27);
             NewPasswordTextbox.TabIndex = 1;
@@ -57,42 +60,53 @@
             // 
             AdminCheckbox.AutoSize = true;
             AdminCheckbox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            AdminCheckbox.Location = new Point(301, 209);
+            AdminCheckbox.Location = new Point(511, 28);
             AdminCheckbox.Name = "AdminCheckbox";
-            AdminCheckbox.Size = new Size(193, 25);
+            AdminCheckbox.Size = new Size(139, 25);
             AdminCheckbox.TabIndex = 2;
-            AdminCheckbox.Text = "Enable EnableAdmin";
+            AdminCheckbox.Text = "Enable Admin";
             AdminCheckbox.UseVisualStyleBackColor = true;
             // 
             // CreateButton
             // 
             CreateButton.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            CreateButton.Location = new Point(301, 558);
+            CreateButton.Location = new Point(298, 685);
             CreateButton.Name = "CreateButton";
             CreateButton.Size = new Size(196, 43);
             CreateButton.TabIndex = 3;
-            CreateButton.Text = "Create";
+            CreateButton.Text = "Create User";
             CreateButton.UseVisualStyleBackColor = true;
             CreateButton.Click += CreateButton_Click;
             // 
             // DescriptionTextbox
             // 
             DescriptionTextbox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            DescriptionTextbox.Location = new Point(207, 285);
+            DescriptionTextbox.Location = new Point(16, 26);
             DescriptionTextbox.Multiline = true;
             DescriptionTextbox.Name = "DescriptionTextbox";
             DescriptionTextbox.PlaceholderText = "Description (Optional)";
-            DescriptionTextbox.Size = new Size(376, 219);
+            DescriptionTextbox.Size = new Size(376, 380);
             DescriptionTextbox.TabIndex = 4;
+            // 
+            // OptionsControlPanel
+            // 
+            OptionsControlPanel.Controls.Add(DescriptionTextbox);
+            OptionsControlPanel.Controls.Add(AdminCheckbox);
+            OptionsControlPanel.Font = new Font("Century Gothic", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            OptionsControlPanel.Location = new Point(12, 200);
+            OptionsControlPanel.Name = "OptionsControlPanel";
+            OptionsControlPanel.Size = new Size(776, 425);
+            OptionsControlPanel.TabIndex = 5;
+            OptionsControlPanel.TabStop = false;
+            OptionsControlPanel.Text = "OPTIONAL";
             // 
             // CreateUserForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 659);
-            Controls.Add(DescriptionTextbox);
+            ClientSize = new Size(800, 768);
+            Controls.Add(OptionsControlPanel);
             Controls.Add(CreateButton);
-            Controls.Add(AdminCheckbox);
             Controls.Add(NewPasswordTextbox);
             Controls.Add(NewUsernameTextbox);
             FormBorderStyle = FormBorderStyle.Fixed3D;
@@ -103,6 +117,8 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Create User";
             Load += CreateUserForm_Load;
+            OptionsControlPanel.ResumeLayout(false);
+            OptionsControlPanel.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -114,5 +130,6 @@
         private CheckBox AdminCheckbox;
         private Button CreateButton;
         private TextBox DescriptionTextbox;
+        private GroupBox OptionsControlPanel;
     }
 }
