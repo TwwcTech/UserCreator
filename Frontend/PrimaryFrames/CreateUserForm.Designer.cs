5f28d5cf7218eec17782c81b6fee4062c92bfa9e
twwct <twwc.tech@gmail.com>
2023-10-22 23:33:51 -0600

    panel size updated

--- Frontend/PrimaryFrames/CreateUserForm.Designer.cs
+++ Frontend/PrimaryFrames/CreateUserForm.Designer.cs
@@ -34,13 +34,15 @@
             CreateButton = new Button();
             DescriptionTextbox = new TextBox();
             OptionsControlPanel = new GroupBox();
+            AccountExpirationTextbox = new CheckBox();
+            MaxBadPasswordTextbox = new CheckBox();
             OptionsControlPanel.SuspendLayout();
             SuspendLayout();
             // 
             // NewUsernameTextbox
             // 
             NewUsernameTextbox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            NewUsernameTextbox.Location = new Point(207, 51);
+            NewUsernameTextbox.Location = new Point(126, 44);
             NewUsernameTextbox.Name = "NewUsernameTextbox";
             NewUsernameTextbox.PlaceholderText = "Enter New User Name";
             NewUsernameTextbox.Size = new Size(376, 27);
@@ -49,7 +51,7 @@
             // NewPasswordTextbox
             // 
             NewPasswordTextbox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            NewPasswordTextbox.Location = new Point(207, 115);
+            NewPasswordTextbox.Location = new Point(126, 108);
             NewPasswordTextbox.Name = "NewPasswordTextbox";
             NewPasswordTextbox.PasswordChar = '*';
             NewPasswordTextbox.PlaceholderText = "Enter New Password";
@@ -60,7 +62,7 @@
             // 
             AdminCheckbox.AutoSize = true;
             AdminCheckbox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            AdminCheckbox.Location = new Point(511, 28);
+            AdminCheckbox.Location = new Point(236, 168);
             AdminCheckbox.Name = "AdminCheckbox";
             AdminCheckbox.Size = new Size(139, 25);
             AdminCheckbox.TabIndex = 2;
@@ -70,7 +72,7 @@
             // CreateButton
             // 
             CreateButton.Font = new Font("Century Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            CreateButton.Location = new Point(298, 685);
+            CreateButton.Location = new Point(223, 564);
             CreateButton.Name = "CreateButton";
             CreateButton.Size = new Size(196, 43);
             CreateButton.TabIndex = 3;
@@ -81,31 +83,55 @@
             // DescriptionTextbox
             // 
             DescriptionTextbox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            DescriptionTextbox.Location = new Point(16, 26);
+            DescriptionTextbox.Location = new Point(6, 26);
             DescriptionTextbox.Multiline = true;
             DescriptionTextbox.Name = "DescriptionTextbox";
             DescriptionTextbox.PlaceholderText = "Description (Optional)";
-            DescriptionTextbox.Size = new Size(376, 380);
+            DescriptionTextbox.Size = new Size(593, 162);
             DescriptionTextbox.TabIndex = 4;
             // 
             // OptionsControlPanel
             // 
+            OptionsControlPanel.Controls.Add(AccountExpirationTextbox);
+            OptionsControlPanel.Controls.Add(MaxBadPasswordTextbox);
             OptionsControlPanel.Controls.Add(DescriptionTextbox);
-            OptionsControlPanel.Controls.Add(AdminCheckbox);
             OptionsControlPanel.Font = new Font("Century Gothic", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            OptionsControlPanel.Location = new Point(12, 200);
+            OptionsControlPanel.Location = new Point(12, 215);
             OptionsControlPanel.Name = "OptionsControlPanel";
-            OptionsControlPanel.Size = new Size(776, 425);
+            OptionsControlPanel.Size = new Size(605, 311);
             OptionsControlPanel.TabIndex = 5;
             OptionsControlPanel.TabStop = false;
             OptionsControlPanel.Text = "OPTIONAL";
+            // 
+            // AccountExpirationTextbox
+            // 
+            AccountExpirationTextbox.AutoSize = true;
+            AccountExpirationTextbox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            AccountExpirationTextbox.Location = new Point(6, 272);
+            AccountExpirationTextbox.Name = "AccountExpirationTextbox";
+            AccountExpirationTextbox.Size = new Size(180, 25);
+            AccountExpirationTextbox.TabIndex = 6;
+            AccountExpirationTextbox.Text = "Account Expiration";
+            AccountExpirationTextbox.UseVisualStyleBackColor = true;
+            // 
+            // MaxBadPasswordTextbox
+            // 
+            MaxBadPasswordTextbox.AutoSize = true;
+            MaxBadPasswordTextbox.Font = new Font("Century Gothic", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            MaxBadPasswordTextbox.Location = new Point(6, 214);
+            MaxBadPasswordTextbox.Name = "MaxBadPasswordTextbox";
+            MaxBadPasswordTextbox.Size = new Size(254, 25);
+            MaxBadPasswordTextbox.TabIndex = 5;
+            MaxBadPasswordTextbox.Text = "Max Bad Password Attempts";
+            MaxBadPasswordTextbox.UseVisualStyleBackColor = true;
             // 
             // CreateUserForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 768);
+            ClientSize = new Size(629, 645);
             Controls.Add(OptionsControlPanel);
+            Controls.Add(AdminCheckbox);
             Controls.Add(CreateButton);
             Controls.Add(NewPasswordTextbox);
             Controls.Add(NewUsernameTextbox);
@@ -131,5 +157,7 @@
         private Button CreateButton;
         private TextBox DescriptionTextbox;
         private GroupBox OptionsControlPanel;
+        private CheckBox MaxBadPasswordTextbox;
+        private CheckBox AccountExpirationTextbox;
     }
 }
