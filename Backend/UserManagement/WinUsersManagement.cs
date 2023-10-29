21a256013a0f751d9583d2a04e78bfc63c6f1687
twwct <twwc.tech@gmail.com>
2023-10-29 12:00:27 -0600

    logic updated

--- Backend/UserManagement/WinUsersManagement.cs
+++ Backend/UserManagement/WinUsersManagement.cs
@@ -1,18 +1,16 @@
-﻿using System.DirectoryServices;
+﻿using System.DirectoryServices.AccountManagement;
 using System.Text.RegularExpressions;
 
 namespace UserCreator.Backend.UserManagement
 {
     internal partial class WinUsersManagement
     {
-        private readonly string _localMachineEnvironement = $"WinNT://{Environment.MachineName},computer";
         private readonly Regex _lowercase = MyRegex();
 
         private string? _username;
         private string? _password;
         private bool _enableAdmin = false;
         private string? _description;
-        private int _maxBadPassword;
         private DateTime _accountExpirationDate;
         private string[]? _localUsers;
 
@@ -43,12 +41,6 @@ namespace UserCreator.Backend.UserManagement
             set => _description = value;
         }
 
-        public int MaxBadPassword
-        {
-            get => _maxBadPassword;
-            set => _maxBadPassword = value;
-        }
-
         public DateTime AccountExpirationLength
         {
             get => _accountExpirationDate;
@@ -63,154 +55,114 @@ namespace UserCreator.Backend.UserManagement
 
         public void CreateUser()
         {
-            using ()
-
-            //using (DirectoryEntry localMachine = new(_localMachineEnvironement))
-            //{
-            //    using (DirectoryEntry newUser = localMachine.Children.Add(Username, "User"))
-            //    {
-            //        try
-            //        {
-            //            newUser.Invoke("SetPassword", new object[] { Password });
-
-            //            if (!string.IsNullOrWhiteSpace(Description))
-            //            {
-            //                newUser.Invoke("Put", new object[] { "Description", Description });
-            //            }
-
-            //            if (!int.IsNegative(MaxBadPassword) || MaxBadPassword != default)
-            //            {
-            //                newUser.Properties["LockoutThreshold"].Value = MaxBadPassword;
-            //            }
-
-            //            if (AccountExpirationLength != default || AccountExpirationLength != DateTime.Today)
-            //            {
-            //                const long maxDate = 0x7FFFFFFFFFFFFFFF;
-            //                newUser.Properties["AccountExpires"].Value = AccountExpirationLength.ToFileTime() > DateTime.MaxValue.ToFileTime() - maxDate ? maxDate : AccountExpirationLength.ToFileTime();
-            //            }
-
-            //            newUser.CommitChanges();
-            //        }
-            //        catch (Exception ex)
-            //        {
-            //            throw new Exception(ex.ToString());
-            //        }
-            //    }
-
-                if (EnableAdmin)
-                {
-                    using (DirectoryEntry adminGroup = localMachine.Children.Find("Administrators", "group"))
-                    {
-                        try
-                        {
-                            adminGroup.Invoke("Add", new object[] { $"{localMachine.Path}/{Username}" });
-                            adminGroup.CommitChanges();
-                        }
-                        catch (Exception ex)
-                        {
-                            throw new Exception(ex.ToString());
-                        }
-                    }
-                }
-            }
-        }
-
-        public void UpdatePassword()
-        {
-            using (DirectoryEntry localMachine = new(_localMachineEnvironement))
+            using PrincipalContext context = new(ContextType.Machine);
+            UserPrincipal user = new(context)
             {
-                using (DirectoryEntry userPasswordUpdate = localMachine.Children.Find(Username))
-                {
-                    if (userPasswordUpdate != null)
-                    {
-                        userPasswordUpdate.Invoke("SetPassword", new object[] { Password });
-                        userPasswordUpdate.CommitChanges();
-                    }
-                }
-            }
+                SamAccountName = Username,
+                Enabled = true,
+                Description = !string.IsNullOrWhiteSpace(Description) ? Description : string.Empty,
+                AccountExpirationDate = AccountExpirationLength != default || AccountExpirationLength != DateTime.Today ? AccountExpirationLength : null
+            };
+            user.SetPassword(Password);
+            user.Save();
         }
 
-        public void UpdateDescription()
-        {
-            using (DirectoryEntry localMachine = new(_localMachineEnvironement))
-            {
-                using (DirectoryEntry userDescriptionUpdate = localMachine.Children.Find(Username))
-                {
-                    if (userDescriptionUpdate != null)
-                    {
-                        userDescriptionUpdate.Properties["Description"].Value = Description;
-                        userDescriptionUpdate.CommitChanges();
-                    }
-                }
-            }
-        }
+        //public void UpdatePassword()
+        //{
+        //    using (DirectoryEntry localMachine = new(_localMachineEnvironement))
+        //    {
+        //        using (DirectoryEntry userPasswordUpdate = localMachine.Children.Find(Username))
+        //        {
+        //            if (userPasswordUpdate != null)
+        //            {
+        //                userPasswordUpdate.Invoke("SetPassword", new object[] { Password });
+        //                userPasswordUpdate.CommitChanges();
+        //            }
+        //        }
+        //    }
+        //}
 
-        public void UpdateMaxBadPasswords()
-        {
-            using (DirectoryEntry localMachine = new(_localMachineEnvironement))
-            {
-                using (DirectoryEntry maxBadPassUpdate = localMachine.Children.Find(Username))
-                {
-                    if (maxBadPassUpdate != null)
-                    {
-                        maxBadPassUpdate.Properties["LockoutThreshold"].Value = MaxBadPassword;
-                        maxBadPassUpdate.CommitChanges();
-                    }
-                }
-            }
-        }
+        //public void UpdateDescription()
+        //{
+        //    using (DirectoryEntry localMachine = new(_localMachineEnvironement))
+        //    {
+        //        using (DirectoryEntry userDescriptionUpdate = localMachine.Children.Find(Username))
+        //        {
+        //            if (userDescriptionUpdate != null)
+        //            {
+        //                userDescriptionUpdate.Properties["Description"].Value = Description;
+        //                userDescriptionUpdate.CommitChanges();
+        //            }
+        //        }
+        //    }
+        //}
 
-        public void UpdateAccountExpirationDate()
-        {
-            using (DirectoryEntry localMachine = new(_localMachineEnvironement))
-            {
-                using (DirectoryEntry accountExpirationUpdate = localMachine.Children.Find(Username))
-                {
-                    if (accountExpirationUpdate != null)
-                    {
-                        const long maxDate = 0x7FFFFFFFFFFFFFFF;
-                        accountExpirationUpdate.Properties["AccountExpires"].Value = AccountExpirationLength.ToFileTime() > DateTime.MaxValue.ToFileTime() - maxDate ? maxDate : AccountExpirationLength.ToFileTime();
-                        accountExpirationUpdate.CommitChanges();
-                    }
-                }
-            }
-        }
+        //public void UpdateMaxBadPasswords()
+        //{
+        //    using (DirectoryEntry localMachine = new(_localMachineEnvironement))
+        //    {
+        //        using (DirectoryEntry maxBadPassUpdate = localMachine.Children.Find(Username))
+        //        {
+        //            if (maxBadPassUpdate != null)
+        //            {
+        //                maxBadPassUpdate.Properties["LockoutThreshold"].Value = MaxBadPassword;
+        //                maxBadPassUpdate.CommitChanges();
+        //            }
+        //        }
+        //    }
+        //}
 
-        public void DeleteUser()
-        {
-            using (DirectoryEntry localMachine = new(_localMachineEnvironement))
-            {
-                using (DirectoryEntry userToDelete = localMachine.Children.Find(Username))
-                {
-                    if (userToDelete != null)
-                    {
-                        try
-                        {
-                            localMachine.Children.Remove(userToDelete);
-                            localMachine.CommitChanges();
-                        }
-                        catch (Exception ex)
-                        {
-                            throw new Exception(ex.ToString());
-                        }
-                    }
-                }
-            }
-        }
+        //public void UpdateAccountExpirationDate()
+        //{
+        //    using (DirectoryEntry localMachine = new(_localMachineEnvironement))
+        //    {
+        //        using (DirectoryEntry accountExpirationUpdate = localMachine.Children.Find(Username))
+        //        {
+        //            if (accountExpirationUpdate != null)
+        //            {
+        //                const long maxDate = 0x7FFFFFFFFFFFFFFF;
+        //                accountExpirationUpdate.Properties["AccountExpires"].Value = AccountExpirationLength.ToFileTime() > DateTime.MaxValue.ToFileTime() - maxDate ? maxDate : AccountExpirationLength.ToFileTime();
+        //                accountExpirationUpdate.CommitChanges();
+        //            }
+        //        }
+        //    }
+        //}
 
-        public void GetLocalWindowsUsers()
-        {
-            using (DirectoryEntry localMachine = new(_localMachineEnvironement))
-            {
-                foreach (DirectoryEntry entry in localMachine.Children)
-                {
-                    if (entry.SchemaClassName == "User")
-                    {
-                        LocalUsers = new string[] { entry.Name };
-                    }
-                }
-            }
-        }
+        //public void DeleteUser()
+        //{
+        //    using (DirectoryEntry localMachine = new(_localMachineEnvironement))
+        //    {
+        //        using (DirectoryEntry userToDelete = localMachine.Children.Find(Username))
+        //        {
+        //            if (userToDelete != null)
+        //            {
+        //                try
+        //                {
+        //                    localMachine.Children.Remove(userToDelete);
+        //                    localMachine.CommitChanges();
+        //                }
+        //                catch (Exception ex)
+        //                {
+        //                    throw new Exception(ex.ToString());
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+        //public void GetLocalWindowsUsers()
+        //{
+        //    using (DirectoryEntry localMachine = new(_localMachineEnvironement))
+        //    {
+        //        foreach (DirectoryEntry entry in localMachine.Children)
+        //        {
+        //            if (entry.SchemaClassName == "User")
+        //            {
+        //                LocalUsers = new string[] { entry.Name };
+        //            }
+        //        }
+        //    }
+        //}
 
         [GeneratedRegex("[a-z]")]
         private static partial Regex MyRegex();
