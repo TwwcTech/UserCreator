ce99757e3c3ebf4bfb1a730406fc7ec4fa743f26
twwct <twwc.tech@gmail.com>
2023-10-29 20:39:32 -0600

    checkbox items disabled on startup and enabled when checked

--- Frontend/PrimaryFrames/UpdateUserForm.cs
+++ Frontend/PrimaryFrames/UpdateUserForm.cs
@@ -12,18 +12,37 @@ namespace UserCreator.Frontend.PrimaryFrames
         private void UpdateUserForm_Load(object sender, EventArgs e)
         {
             ActiveControl = UpdateButton;
+
             WinUsersManagement localUserAccounts = new();
             localUserAccounts.GetLocalWindowsUsers();
-
             foreach (string userAccount in localUserAccounts.LocalUsers)
             {
                 UsersCombobox.Items.Add(userAccount);
             }
+
+            UpdatePassTextbox.Enabled = false;
+            UpdateDescTextbox.Enabled = false;
+            UpdateDateTimePicker.Enabled = false;
         }
 
         private void UpdateButton_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void UpdatePassCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdatePassTextbox.Enabled = UpdatePassCheckbox.Checked;
+        }
+
+        private void UpdateDescCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateDescTextbox.Enabled = UpdateDescCheckbox.Checked;
+        }
+
+        private void UpdateAcctExpCheckbox_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateDateTimePicker.Enabled = UpdateAcctExpCheckbox.Checked;
         }
     }
 }
