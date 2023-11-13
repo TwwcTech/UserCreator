eedf12957f46d6957965bb35d91ee963ba76797b
twwct <twwc.tech@gmail.com>
2023-11-12 21:02:04 -0700

    main icon added

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             CreateUserButton = new Button();
             UpdateUserButton = new Button();
             DeleteUserButton = new Button();
@@ -89,6 +90,7 @@
             ClientSize = new Size(825, 450);
             Controls.Add(ConsoleButtonsPanel);
             FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MaximizeBox = false;
             Name = "MainForm";
             SizeGripStyle = SizeGripStyle.Hide;
